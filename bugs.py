8a377f740c830f4261455d2ac50b3fed9a368e5b
sztankatt <sztankatt@gmail.com>
2016-01-18 20:31:09 +0000

    fixed variable name bug

--- bugs.py
+++ bugs.py
@@ -3,15 +3,15 @@ import sys
 import parsing
 import cache
 import filemanagement as fm
+import git
 from git import Repo
 from consants import FACEBOOK_SDK_REPO
 
 
 class Repository():
-    def __init__(self, repo_dir, cache_size=4, distance_to_fetch=1):
+    def __init__(self, repo_dir, cache_ratio=0.5, distance_to_fetch=1):
         self.file_distances = fm.DistanceSet()
         self.file_set = fm.FileSet()
-        self.cache = cache.SimpleCache(cache_size)
         self.commit_order = {}
         self.distance_to_fetch = distance_to_fetch
         self.hit_count = 0
@@ -22,20 +22,25 @@ class Repository():
         self.commit_list = list(reversed(
             list(self.repo.iter_commits('master'))))
         commit_counter = 0
+	
+	cache_size = int(cache_ratio*float(self._get_file_count(self.commit_list[-1])))
+	print cache_size
+        self.cache = cache.SimpleCache(16)
+
         for commit in self.commit_list:
             self.commit_order[commit.hexsha] = commit_counter
             commit_counter += 1
         for commit in self.commit_list:
-            parents = commit.parents
-            if len(parents) == 1:
+	    print self.cache.file_set
+	    print self.cache._filled()
+	    parents = commit.parents
+	    if len(parents) == 1:
                 files = self.get_diff_file_list(commit, parents[0])
-                created_files = filter(
-                    lambda x: x[0] == 'creation', files)
-                changed_files = filter(lambda x: x[0] == 'change', files)
+                created_files = filter(lambda x: x[0] == 'created', files)
+                changed_files = filter(lambda x: x[0] == 'changed', files)
 
-                # print changed_files
                 if parsing.is_fix_commit(commit.message):
-                    for _, path, del_lines in changed_files:
+		    for _, path, del_lines in changed_files:
                         file_ = self.file_set.get_file(path)
                         file_.fault(self.commit_order[commit.hexsha])
                         if self.cache.file_in(file_):
@@ -43,19 +48,24 @@ class Repository():
                         else:
                             self.miss_count += 1
                             self.cache.add(file_)
-
-                        line_intr_c = self.get_line_introducting_commits(
+                        line_intr_c = self.get_line_introducing_commits(
                             del_lines, path, commit)
-
-                        print line_intr_c
+			
+			for c in line_intr_c:
+			    closest_files = self.file_distances.get_closest_files(
+				file_,
+				self.distance_to_fetch,
+				self.commit_order[c.hexsha])
+			    self.cache.add_multiple(closest_files)
+			
                 else:
-                    for _, path, l in changed_files:
+		    for _, path, l in changed_files:
                         file_ = self.file_set.get_file(
                             path)
 
                         file_.changed(self.commit_order[commit.hexsha])
 
-                        self.cache.add(file_)
+			self.cache.add(file_)
                 for _, path, l in created_files:
                     self.cache.add(self.file_set.get_file(
                         path, self.commit_order[commit.hexsha]))
@@ -64,12 +74,16 @@ class Repository():
                 # initial commit
                 files = self.get_commit_tree_files(commit)
                 self.cache.add_multiple(self.file_set.get_multiple(files))
+            else:
+		pass
 
             commit_counter += 1
             """For each fixing commit: check files in cache.
             If miss, get the bug introducing commits for that file,
             at the parents commit point in time.
             """
+    def _get_file_count(self, commit):
+      return len(self.get_commit_tree_files(commit))
 
     def get_deleted_lines(self, diff_message):
         """returns the line numbers which were deleted by a commit
@@ -89,7 +103,7 @@ class Repository():
         for commit, lines in self.repo.blame(commit, file_):
             line_count += len(lines)
 
-    def get_line_introducting_commits(self, line_list, file_, commit):
+    def get_line_introducing_commits(self, line_list, file_, commit):
         """Returns the set of commits which introduced lines in a file.
         Ideally this will be a single commit, not more.
 
@@ -106,13 +120,18 @@ class Repository():
         :rtype: Commit set
         :return: Returns the set of commits which introduced the line_list
         """
-
+	#print commit.hexsha
         commit_list = []
         commit_set = []
-        for commit, lines in self.repo.blame(commit, file_):
-            commit_list += [(commit, x) for x in lines]
-
-        print commit_list[1]
+        try:
+	    for commit, lines in self.repo.blame(commit, file_):
+		commit_list += [(commit, x) for x in lines]
+	except git.exc.GitCommandError:
+	    pass
+	    print file_ + " " + commit.hexsha
+	finally:
+	    pass
+	    #print "\n\n"
 
         for start_line, end_line in line_list:
             for commit, line in commit_list[start_line:start_line+end_line+1]:
@@ -134,21 +153,19 @@ class Repository():
         :return: Returns a list of tuples with specification as above
         """
 
-        diffs = commit1.diff(commit2, create_patch=True, unified=True)
-
-        deleted_lines = self.get_deleted_lines(diffs[0].diff.splitlines())
+        diffs = commit2.diff(commit1, create_patch=True, unified=True)
 
         file_list = []
         for diff in diffs:
-            if diff.a_blob is None and diff.b_blob is not None:
-                # new file
-                file_list.append(('creation', diff.b_blob.path, deleted_lines))
-            elif diff.a_blob is not None and diff.b_blob is None:
-                # deleted file
-                file_list.append(('deletion', diff.a_blob.path, deleted_lines))
-            elif diff.a_blob is not None and diff.b_blob is not None:
-                # changed file
-                file_list.append(('change', diff.b_blob.path, deleted_lines))
+	    deleted_lines = self.get_deleted_lines(diff.diff.splitlines())
+	    if diff.new_file:
+		file_list.append(('created', diff.b_blob.path, deleted_lines))
+	    elif diff.deleted_file:
+		file_list.append(('deleted', diff.a_blob.path, deleted_lines))
+	    elif diff.renamed:
+		print "renamed"
+	    else:
+		file_list.append(('changed', diff.b_blob.path, deleted_lines))
 
         return file_list
 
@@ -184,11 +201,6 @@ class Repository():
 def main():
     """Main entry point for the script."""
     r = Repository(FACEBOOK_SDK_REPO)
-    c = r.get_line_introducting_commits([(1, 1)], '.travis.yml',
-        r.commit_list[-1]).pop()
-    r.get_diff_file_list(c, c.parents[0])
-
-    print r.commit_list[0].message
 
     print r.hit_count
     print r.miss_count
