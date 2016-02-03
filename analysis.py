f5f65d899c030a1ef65ad72f391753001650bd96
Tamas Sztanka-Toth <sztankatt@gmail.com>
2016-02-03 20:02:57 +0000

    Added new analysis

--- analysis.py
+++ analysis.py
@@ -71,14 +71,49 @@ def analyse_by_cache_ratio(repo, dtf, pfs, progressive=True):
     logger.info("Analysis finished at %s\n" % (datetime.datetime.now(),))
 
 
+def analyse_by_pfs_dtf(repo, cache_ratio, pfs_set, dtf_set):
+    """Analyse by pfs and dtf."""
+    logger.info(
+        "Starting fixcache for fixed cache of %s at %s" %
+        (cache_ratio, datetime.datetime.now()))
+    dir_ = os.path.join(constants.CSV_ROOT, repo.repo_dir)
+
+    if not os.path.exists(dir_):
+        os.makedirs(dir_)
+
+    file_ = os.path.join(
+        dir_, 'analyse_by_fixed_cache_%s.csv' % (cache_ratio,))
+
+    if os.path.exists(file_):
+        logger.info('Analysis exists.\nExit\n')
+        return
+
+    with open(file_, 'wb') as out:
+        csv_out = csv.writer(out)
+        csv_out.writerow(
+            ['repo_dir', 'hits', 'misses', 'cache_size', 'dtf', 'pfs', 'ttr'])
+
+        for pfs in pfs_set:
+            for dtf in dtf_set:
+                logging.info(
+                    ('Running fixcache for %s with ratio of %s and dtf ' +
+                     'of %s, with pfs of %s') %
+                    (repo.repo_dir, cache_ratio, dtf, pfs))
+                csv_out.writerow(basic_fixcache_analyser(
+                    repo, cache_ratio, dtf, pfs))
+
+    logger.info("Analysis finished at %s\n" % (datetime.datetime.now(),))
+
+
 def main():
     # facebook_sdk_repo = Repository(constants.FACEBOOK_SDK_REPO)
-    # boto3_repo = Repository(constants.BOTO3_REPO, branch='develop')
+    boto3_repo = Repository(constants.BOTO3_REPO, branch='develop')
     # boto_repo = Repository(constants.BOTO_REPO, branch='develop')
-    raspberry_io_repo = Repository('raspberryio')
+    # raspberry_io_repo = Repository('raspberryio')
 
     # boto3 tests
-    variables = [0.1, 0.15, 0.2, 0.25, 0.3]
+    dtf_set = [(x + 1) * 5 / 100.0 for x in range(20)]
+    pfs_set = [(x + 1) / 100.0 for x in range(20)]
     # for i in variables:
     #     for j in variables:
     #         analyse_by_cache_ratio(facebook_sdk_repo, dtf=i, pfs=j)
@@ -90,13 +125,15 @@ def main():
     # for i in variables:
     #     for j in variables:
     #         analyse_by_cache_ratio(boto_repo, dtf=i, pfs=j)
-    for i in variables:
-        for j in variables:
-            analyse_by_cache_ratio(raspberry_io_repo, dtf=i, pfs=j)
+    # for i in variables:
+    #    for j in variables:
+    #        analyse_by_cache_ratio(raspberry_io_repo, dtf=i, pfs=j)
+    analyse_by_pfs_dtf(
+        boto3_repo, cache_ratio=0.15, pfs_set=pfs_set, dtf_set=dtf_set)
 
     # analyse_by_cache_ratio(boto_repo, 1)
 if __name__ == '__main__':
-    pid = os.path.join(constants.BASE_DIR, 'fixcache2.pid')
+    pid = os.path.join(constants.BASE_DIR, 'fixcache3.pid')
 
     daemon = Daemonize(app="fixcache", pid=pid, action=main, keep_fds=keep_fds)
     daemon.start()
--- constants.py
+++ constants.py
@@ -5,5 +5,6 @@ REPO_DIR = os.path.join(BASE_DIR, 'repos')
 FACEBOOK_SDK_REPO = 'facebook-sdk'
 BOTO3_REPO = 'boto3'
 BOTO_REPO = 'boto'
+AML = 'awesome-machine-learning'
 CSV_ROOT = os.path.join(BASE_DIR, 'fixcache', 'analysis_output')
-LOGFILE = os.path.join(BASE_DIR, 'fixcache', 'logs', 'fixcache.log')
+LOGFILE = os.path.join(BASE_DIR, 'fixcache', 'logs', 'fixcache2.log')
