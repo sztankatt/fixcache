c0d8d554486072ba5d20efc1a11c77dc864d19c2
Tamas Sztanka-Toth <sztankatt@gmail.com>
2016-02-04 01:32:46 +0000

    Added graph files

---
+++ graph_data.py
@@ -0,0 +1,34 @@
+"""Data for plot_1)."""
+graph_data = {
+    'title': 'Figure 4',
+    'curves': [
+        {
+            'args': ['pythonforfacebook/facebook-sdk.git (#c=346)'],
+            'options': {
+                'color': 'blue',
+                'repo': 'facebook-sdk',
+            }
+        },
+        {
+            'args': ['boto/boto.git (#c=6935)'],
+            'options': {
+                'color': 'orange',
+                'repo': 'boto',
+            }
+        },
+        {
+            'args': ['boto/boto3.git (#c=685)'],
+            'options': {
+                'color': 'red',
+                'repo': 'boto3',
+            }
+        },
+        {
+            'args': ['python/raspberryio.git (#c=614)'],
+            'options': {
+                'color': 'green',
+                'repo': 'raspberryio',
+            }
+        },
+    ]
+}
